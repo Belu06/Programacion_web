85dc5f9647cfa2fadc130e79803305a253511f45
Belu06 <beluh065@gmail.com>
2023-08-30 16:58:03 -0300

    Actividades Array y Objetos

---
+++ Belén Huanca-Programación Web-5D/index.js
@@ -0,0 +1,29 @@
+//Arrays:
+
+/*ACTIVIDAD 1:
+
+const numbers = [1, 2, 3, 4, 5]
+numbers[0] = 10
+numbers[2] = 30
+console.log(numbers)
+
+.¿Qué se imprimirá en la consola al ejecutar el siguiente código?
+Se imprimirá [10, 2, 30, 4, 5]
+
+/*ACTIVIDAD 3:
+
+const numbers = [1, 2, 3, 4];
+let suma = 0;
+function sumarPares(numeros) {
+  for (let i = 0; i < numeros.length; i++) {
+    if (numeros[i] % 2 === 0) {
+      suma += numeros[i];
+    }
+  }
+  console.log(suma);
+}
+sumarPares(numbers); */
+
+/*ACTIVIDAD 4:
+
+
---
+++ Belén Huanca-Programación Web-5D/index2.js
@@ -0,0 +1,17 @@
+Objetos:
+/*ACTIVIDAD 1:
+
+function createObject(name, subs) {
+   const persona ={
+    name: name,
+    subscribers: subs,
+    hash: name.lenght * subs,
+    getStatus: function () { // <- método
+        return `El canal de ${name} tiene ${subscribers}`
+    }
+    } 
+} */
+
+//ACTIVIDAD 2:
+
+
